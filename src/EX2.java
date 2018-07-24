d0de5cd586bee7e120471ffb05f7a93c01d7240f
Mike <m.g.johnson0626@gmail.com>
2018-07-24 10:34:48 -0500

    finalcheck

--- src/EX2.java
+++ src/EX2.java
@@ -3,6 +3,29 @@ import java.util.Scanner;
 public class EX2 {
 
     public static void main(String[] args){
+        Scanner scanInput = new Scanner(System.in);
+        String password1, password2= "";
+
+        System.out.print("Enter your new password: ");
+        password1 = scanInput.nextLine();
+
+        System.out.print("Enter new password again: ");
+        password2 = scanInput.nextLine();
+
+        System.out.println("Does passwords match? " + checkPassword(password1, password2));
+    }
+    public static boolean checkPassword(String pw1, String pw2){
+        if (pw1.equals(pw2)){
+
+            return true;
+        }
+
+        else{
+
+        }
+            return false;
+
+
 
     }
 
