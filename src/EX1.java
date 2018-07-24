b869a55d5d52a9b9379bb624120fa844b41685f3
Mike <m.g.johnson0626@gmail.com>
2018-07-24 10:18:53 -0500

    newcheck

---
+++ src/EX1.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class EX1 {
+
+    public static void main(String[] args) {
+        int number1 =10;
+        int number2 = 5;
+        System.out.println("The largest number is:" + largestNumber(number1, number2));
+
+    }
+    public static int largestNumber(int num1, int num2){
+        if (num1 < num2);
+            return num1;
+    }
+}
